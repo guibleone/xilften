3f9b0e48db2347cdcb162aec2fc39d092a6adfe9
Guilherme B. Leone <81646062+guibleone@users.noreply.github.com>
2023-02-03 17:10:21 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import MovieCard from './MovieCard'
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com?apikey=8b25b370'
+
+function App() {
+   
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const searchMovies = async (title) =>{
+    const response =  await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    setMovies(data.Search)
+  }
+
+  useEffect(()=>{
+    searchMovies('Avengers')
+  },[])
+
+  return (
+    <div className='app'>
+       <h1>Xilften</h1>
+
+       <div className='search'>
+        <input 
+        placeholder='Procurar por um filme'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}       
+        />
+
+      <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)} />
+
+       </div>
+
+       {movies?.length > 0 
+          ? ( 
+          <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie = {movie}/>
+              ) )}
+           </div>
+          ) : (
+            <div className='empty'> 
+                <h2>Nenhum filme encontrado</h2>
+            </div>
+        )}
+
+      
+    </div>
+  )
+}
+
+export default App
